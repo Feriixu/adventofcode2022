e0984b339c60c85517808b71b3a580eea81a5417
F. St <39712181+Feriixu@users.noreply.github.com>
2022-12-07 18:17:56 +0100

    Fix mods

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
+use itertools::Itertools;
 use std::fs::read_to_string;
 use std::time::Instant;
-use itertools::Itertools;
 
 mod day1;
 mod day2;
 mod day3;
 mod day4;
 mod day5;
+mod day6;
+mod day7;
 
 fn main() {
     // Start perf counter
@@ -74,15 +76,15 @@ fn main() {
 
     println!("---------- Day 5 ----------");
     // Given representations of the stacks
-//         [H]         [S]         [D]
-//     [S] [C]         [C]     [Q] [L]
-//     [C] [R] [Z]     [R]     [H] [Z]
-//     [G] [N] [H] [S] [B]     [R] [F]
-// [D] [T] [Q] [F] [Q] [Z]     [Z] [N]
-// [Z] [W] [F] [N] [F] [W] [J] [V] [G]
-// [T] [R] [B] [C] [L] [P] [F] [L] [H]
-// [H] [Q] [P] [L] [G] [V] [Z] [D] [B]
-//  1   2   3   4   5   6   7   8   9
+    //         [H]         [S]         [D]
+    //     [S] [C]         [C]     [Q] [L]
+    //     [C] [R] [Z]     [R]     [H] [Z]
+    //     [G] [N] [H] [S] [B]     [R] [F]
+    // [D] [T] [Q] [F] [Q] [Z]     [Z] [N]
+    // [Z] [W] [F] [N] [F] [W] [J] [V] [G]
+    // [T] [R] [B] [C] [L] [P] [F] [L] [H]
+    // [H] [Q] [P] [L] [G] [V] [Z] [D] [B]
+    //  1   2   3   4   5   6   7   8   9
     // equivalent 2D vector representation
     let stacks = vec![
         "HTZD".chars().collect_vec(),
@@ -95,8 +97,20 @@ fn main() {
         "DLVZRHW".chars().collect_vec(),
         "BHGNFZLD".chars().collect_vec(),
     ];
-    println!("Part 1: {}", day5::part1(read_to_string("input/day5/input_instructions.txt").unwrap(), stacks.clone()));
-    println!("Part 2: {}", day5::part2(read_to_string("input/day5/input_instructions.txt").unwrap(), stacks));
+    println!(
+        "Part 1: {}",
+        day5::part1(
+            read_to_string("input/day5/input_instructions.txt").unwrap(),
+            stacks.clone()
+        )
+    );
+    println!(
+        "Part 2: {}",
+        day5::part2(
+            read_to_string("input/day5/input_instructions.txt").unwrap(),
+            stacks
+        )
+    );
 
     // Print perf counter
     println!("Time: {}ms", start.elapsed().as_secs_f64() * 1000.0);
